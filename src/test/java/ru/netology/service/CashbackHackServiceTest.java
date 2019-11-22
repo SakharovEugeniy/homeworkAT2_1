4290e98d08a12c00a213b231753608682f4b9303
Sakharov Eugeniy <Worgwhd@mail.ru>
2019-11-22 15:05:48 +0600

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn1IfAmountIs999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int actual = cashbackHackService.remain(amount);
+        int expected =1;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected =0;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldReturn999IfAmountIs1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected =999;
+        assertEquals(expected,actual);
+    }
+}
